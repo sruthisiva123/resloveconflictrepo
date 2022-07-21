ed2bf3a0ec57f7504e1512bb31fc76d7934c29dd
sruthisiva123 <109644528+sruthisiva123@users.noreply.github.com>
2022-07-21 08:44:00 +0530

    resolve conflict prgm

---
+++ HelloCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpSolutions
+{
+    class HelloCode
+    {
+         static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+            Console.WriteLine("I am sruthi.....how are you");
+
+        }
+    }
+}
+
